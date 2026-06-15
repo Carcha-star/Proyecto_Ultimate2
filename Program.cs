4a68f02585acfa3730135237c11f6ddcc3b16d43
HUMBERTO <hrruiz@uamv.edu.ni>
2026-06-15 10:26:06 -0600

    Menu de capitanes  con 4 para prueba y una validacion

--- Program.cs
+++ Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+Capitan[] capitanes =
+{
+    new Capitan
+    {
+        Seleccion = "Argentina",
+        Nombre = "Lionel Messi",
+        Edad = 39,
+        Goles = 112,
+        ClubActual = "Inter Miami"
+    },
+    new Capitan
+    {
+        Seleccion = "Portugal",
+        Nombre = "Cristiano Ronaldo",
+        Edad = 41,
+        Goles = 138,
+        ClubActual = "Al Nassr"
+    },
+    new Capitan
+    {
+        Seleccion = "Francia",
+        Nombre = "Kylian Mbappé",
+        Edad = 28,
+        Goles = 52,
+        ClubActual = "Real Madrid"
+    },
+    new Capitan
+    {
+        Seleccion = "Croacia",
+        Nombre = "Luka Modrić",
+        Edad = 41,
+        Goles = 28,
+        ClubActual = "Real Madrid"
+    }
+};
+
+if (capitanes == null || capitanes.Length == 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Error crítico: No se encontraron datos de capitanes cargados.");
+    Console.ResetColor();
+    return;
+}
+
+MostrarMenu(capitanes);
